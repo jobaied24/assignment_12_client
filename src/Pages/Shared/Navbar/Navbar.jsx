0eb1b799eda2f5c9a015365465c35de2da4c2892
hero00 <jobaied543@gmail.com>
2026-06-08 01:13:27 +0600

    adding banner section

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -5,14 +5,14 @@ import CampCureLogo from '../CampLogo/CampCureLogo';
 const Navbar = () => {
 
   const nav = <>
-       <li><NavLink to='/'>Home</NavLink></li>
-       <li><NavLink to='/about'>About Us</NavLink></li>
+       <li><NavLink to='/'>HOME</NavLink></li>
+       <li><NavLink to='/about'>ABOUT US</NavLink></li>
   </>
 
 
 
     return (
-<div className="navbar bg-base-100 shadow-sm px-5">
+<div className="navbar bg-base-100 shadow-sm px-5 py-3">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
