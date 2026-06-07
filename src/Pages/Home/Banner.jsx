533762123e5eb8f2b2e581ac894acad2bac7fdd2
hero00 <jobaied543@gmail.com>
2026-06-08 02:33:20 +0600

    added banner image in banner section

--- src/Pages/Home/Banner.jsx
+++ src/Pages/Home/Banner.jsx
@@ -1,4 +1,3 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import bannerImg1 from '../../assets/doctor1.jpg'
 import bannerImg2 from '../../assets/vaccination.jpg'
@@ -8,20 +7,30 @@ import bannerImg3 from '../../assets/treatment.jpg'
 
 const Banner = () => {
     return (
-        <Carousel autoPlay={true}>
+        <div className='w-full h-screen overflow-hidden'>
+        <Carousel
+         autoPlay
+         interval={5000}
+         infiniteLoop={true}
+         transitionTime={1000}
+         swipeable={false}
+         emulateTouch={false}
+         stopOnHover={false}
+         showIndicators={false}
+         showThumbs={false}
+         showStatus={false}>
             <div>
-                <img src={bannerImg1} />
-                <p className="legend">Legend 1</p>
+                <img src={bannerImg1} className='w-full h-screen object-cover' />
             </div>
             <div>
-                <img src={bannerImg2} />
-                <p className="legend">Legend 2</p>
+                <img src={bannerImg2} className='w-full h-screen object-cover' />
             </div>
             <div>
-                <img src={bannerImg3} />
-                <p className="legend">Legend 3</p>
+                <img src={bannerImg3} className='w-full h-screen object-cover' />
             </div>
         </Carousel>
+        </div>
+
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from "react-router/dom";
 import { router } from './router/router.jsx';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
