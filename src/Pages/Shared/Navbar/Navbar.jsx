93322283cc7be4c264cf186b22817b3f49405cfb
hero00 <jobaied543@gmail.com>
2026-06-11 17:51:47 +0600

    added footer section

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
 
     return (
-<div className="navbar bg-base-100 shadow-sm px-5 py-3">
+<div className="navbar sticky top-0 z-50 bg-base-100 shadow-sm px-5 py-3">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
